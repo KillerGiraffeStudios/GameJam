ef501f6dbe5aed5a63babf0c17d3c9918bc1cc9a
aj3x <ajrussell94@hotmail.com>
2016-02-19 17:50:40 -0600

    MoveScript
    +movement for any rigid body type

---
+++ GameJam2016/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+    int max_speed = 5;
+    int move_force = 5;
+    Rigidbody2D r_body;
+	// Use this for initialization
+	void Start () {
+        r_body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0.1 && r_body.velocity.x<max_speed) {
+            r_body.AddForce(new Vector2(Input.GetAxis("Horizontal") * move_force, 0));
+        }
+	}
+}
