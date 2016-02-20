1dec3c30dfb3603e2941905983e8e1b2b48f296b
aj3x <ajrussell94@hotmail.com>
2016-02-19 22:25:20 -0600

    Updates

--- GameJam2016/Assets/Scripts/CollisionDelegate.cs
+++ GameJam2016/Assets/Scripts/CollisionDelegate.cs
@@ -17,12 +17,7 @@ public class CollisionDelegate : MonoBehaviour {
 	}
 
     void OnEnterCollision2D(Collider2D coll) {
-        //Gets hit by ranged attack
-        if (coll.tag.Equals("Ranged")) {
-            coll.gameObject.SendMessage("damage",coll.gameObject);
-            Destroy(coll.gameObject);
-
-        }else if (coll.tag.Equals("Melee")) {
+		if (coll.tag.Equals("Melee")) {
             coll.gameObject.SendMessage("damage", coll.gameObject);
         }
     }
