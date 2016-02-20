c91515ec8c47723bbb923e5f46c6c841484a30a5
Darvin Zhang <ddz369@mail.usask.ca>
2016-02-19 18:43:31 -0600

    Darvin - First Commit
    +My Scene
    +Menu GUI basics
    +Start Button
    +GameSettings - Global settings of the game

---
+++ GameJam2016/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour {
+
+	//the number of players in the game - may not be needed
+	public int players;
+
+	private static GameSettings _instance;
+	public static GameSettings Instance
+	{
+		get
+		{
+			if (GameSettings._instance == null){
+				GameSettings._instance = new GameObject("GameSettings").AddComponent<GameSettings>();
+				DontDestroyOnLoad(GameSettings._instance);
+			}
+			return GameSettings._instance;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
