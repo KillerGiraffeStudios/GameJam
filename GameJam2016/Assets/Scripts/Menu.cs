c7becc340452c87c977e58f591cf1ac87a514659
Darvin Zhang <ddz369@mail.usask.ca>
2016-02-20 16:45:11 -0600

    Added additional coloring scheme for player selection

--- GameJam2016/Assets/Scripts/Menu.cs
+++ GameJam2016/Assets/Scripts/Menu.cs
@@ -13,7 +13,7 @@ public class Menu : MonoBehaviour {
 	bool canStart = false;
 
 	//the texture of our buttons
-	Texture2D p1, p2, p3, p4, start, create;
+	Texture2D p1, p2, p3, p4, p1c, p2c, p3c, p4c, start, create;
 
 	//initialize all fancy GUI
 	void Start() {
@@ -22,6 +22,10 @@ public class Menu : MonoBehaviour {
 		p2 = Resources.Load ("TwoPlayer") as Texture2D;
 		p3 = Resources.Load ("ThreePlayer") as Texture2D;
 		p4 = Resources.Load ("FourPlayer") as Texture2D;
+		p1c = Resources.Load ("p1change") as Texture2D;
+		p2c = Resources.Load ("p2change") as Texture2D;
+		p3c = Resources.Load ("p3change") as Texture2D;
+		p4c = Resources.Load ("p4change") as Texture2D;
 		start = Resources.Load ("StartMenu") as Texture2D;
 		create = Resources.Load ("CreateMatch") as Texture2D;
 	}
@@ -31,6 +35,14 @@ public class Menu : MonoBehaviour {
 			ShowButton = false;
 		}
 		this.OnGUI();
+	}
+
+	//helper function that resets each button texture to its original form
+	void resetColors() {
+		p1 = Resources.Load ("OnePlayer") as Texture2D;
+		p2 = Resources.Load ("TwoPlayer") as Texture2D;
+		p3 = Resources.Load ("ThreePlayer") as Texture2D;
+		p4 = Resources.Load ("FourPlayer") as Texture2D; 
 	}
 
 	//the gui of the the main menu 
@@ -48,23 +60,31 @@ public class Menu : MonoBehaviour {
 		if (!ShowButton) {	
 			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 3 - 100, Screen.height / 2, 200, 100), p1)) {
 				GameSettings.Instance.players = 1;
-				p1 = start;
+				resetColors ();
+				p1 = p1c;
 				canStart = true;
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 9 - 100, Screen.height / 2, 200, 100), p2)) {
 				GameSettings.Instance.players = 2;
+				resetColors ();
+				p2 = p2c;
 				canStart = true;
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 9 - 100, Screen.height / 2, 200, 100), p3)) {
 				GameSettings.Instance.players = 3;
+				resetColors ();
+				p3 = p3c;
 				canStart = true;
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 3 - 100, Screen.height / 2, 200, 100), p4)) {
 				GameSettings.Instance.players = 4;
+				resetColors ();
+				p4 = p4c;
 				canStart = true;
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 + 90, 150, 75), create) && canStart == true) {
 				GameSettings.Instance.matchSet = true;	//set this to true
+				resetColors ();							//make sure that everything is reset
 				canStart = false;						//reset the match settings for new game
 				Application.LoadLevel (1);				//comment this - will load the level once the settings are good		
 			}
