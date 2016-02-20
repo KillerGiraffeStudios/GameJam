7313dfa7d1efef2ef957e990684caafcf9626cb2
aj3x <ajrussell94@hotmail.com>
2016-02-19 19:29:46 -0600

    Scripts and Tags

---
+++ GameJam2016/Assets/Scripts/CollisionDelegate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Delegate for Player based collisions
+/// </summary>
+public class CollisionDelegate : MonoBehaviour {
+    PlayerClass pClass;
+	// Use this for initialization
+	void Start () {
+        pClass = GetComponent<PlayerClass>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnterCollision2D(Collider2D coll) {
+        //Gets hit by ranged attack
+        if (coll.tag.Equals("Ranged")) {
+            coll.gameObject.SendMessage("damage",coll.gameObject);
+            Destroy(coll.gameObject);
+
+        }else if (coll.tag.Equals("Melee")) {
+            coll.gameObject.SendMessage("damage", coll.gameObject);
+        }
+    }
+}
---
+++ GameJam2016/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+
+    string class_name;
+    delegate void class_delegate();
+	// Use this for initialization
+	void Start () {
+        class_name = gameObject.tag+" ";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public string getName() {
+        return class_name;
+    }
+}
