663217f2d38f2b2c8ff16e0feeaa307c9513585e
aj3x <ajrussell94@hotmail.com>
2016-02-20 14:07:10 -0600

    Button mappings

--- GameJam2016/Assets/Scripts/PlayerClass.cs
+++ GameJam2016/Assets/Scripts/PlayerClass.cs
@@ -5,7 +5,10 @@ public class PlayerClass : MonoBehaviour {
 
     string class_name;
     delegate void buttonDelegate();
-	buttonDelegate bMap;
+	//Based on xbox controller
+	buttonDelegate xMap;//mapping for x button
+	buttonDelegate yMap;//mapping for y button
+	buttonDelegate bMap;//mapping for b button
 	// Use this for initialization
 	void Start () {
         class_name = gameObject.tag+" ";
@@ -13,7 +16,7 @@ public class PlayerClass : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
     public string getName() {
@@ -21,29 +24,72 @@ public class PlayerClass : MonoBehaviour {
     }
 
 	void setMapping(){
+		string player = gameObject.name;
+		xMap = attack;
 		if (class_name.Equals ("Knight")) {
-			
+			yMap = knight_ground;
+			bMap = knight_block;
+		} else if (class_name.Equals ("Ranger")) {
+			yMap = ranger_short;
+			bMap = ranger_long;
+		} else if (class_name.Equals ("Berserker")) {
+
+		} else if (class_name.Equals ("Wizard")) {
+
+		} else if (class_name.Equals ("Rogue")) {
+
+		} else {
+			print ("no valid class selected");
 		}
 
+	}
+
+	void attack(){
+
+	}
+
+
+	void knight_block(){
+
+	}
+	void knight_ground(){
+
+	}
+
+
+	void ranger_short(){
+
+	}
+	void ranger_long(){
+
+	}
+
+
+
+	void berserk_y(){
+
+	}
+	void berserk_b(){
+
+	}
 
 
 
 
+	void wizard_y(){
 
+	}
+	void wizard_b(){
 
-		if(class_name.Equals("Ranger")){
-
-		}
-
-
-
+	}
 
 
 
 
-		if (class_name.Equals ("Berserker")) {
+	void rogue_y(){
 
-		}
+	}
+	void rogue_vanish(){
 
 	}
 }
