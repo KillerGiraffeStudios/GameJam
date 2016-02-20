339faf3d899e50938a1bb05f88ba78ebaa31e1d0
aj3x <ajrussell94@hotmail.com>
2016-02-19 21:20:14 -0600

    Meta script

---
+++ GameJam2016/Assets/Scripts/meta_script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meta_script : MonoBehaviour {
+	void Awake(){
+		DontDestroyOnLoad (transform.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
