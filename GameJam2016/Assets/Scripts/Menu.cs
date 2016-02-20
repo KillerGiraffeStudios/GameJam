4374b7f1145e96057a8a2ac8be2cc41a3bda36a9
Darvin Zhang <ddz369@mail.usask.ca>
2016-02-20 15:30:20 -0600

    Added textures to Menu
    +Resources Folder
    +Bit-like images as the new buttons and title of the Menu
    +Change Menu code and deleted useless stuff

--- GameJam2016/Assets/Scripts/Menu.cs
+++ GameJam2016/Assets/Scripts/Menu.cs
@@ -1,18 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Menu : MonoBehaviour {
 
 	public Texture buttonTexture;
 	public Texture2D icon;
-
 	//boolean to remove button once clicked
 	bool ShowButton = true;
 
 	//boolean set true if the game settings have been confirmed
 	bool start = false;
 
+	//the texture of our buttons
+	Texture2D p1, p2, p3, p4;
+
+	//initialize all fancy GUI
 	void Start() {
+		//load the button textures to assign to our buttons
+		p1 = Resources.Load ("OnePlayer") as Texture2D;
+		p2 = Resources.Load ("TwoPlayer") as Texture2D;
+		p3 = Resources.Load ("ThreePlayer") as Texture2D;
+		p4 = Resources.Load ("FourPlayer") as Texture2D;
 	}
 
 	void update() {
@@ -23,11 +32,8 @@ public class Menu : MonoBehaviour {
 
 	//the gui of the the main menu 
 	void OnGUI () {
-
 		//set no background to the button
 		GUI.backgroundColor = Color.clear;
-		GUI.skin.button.fontSize = 15;
-		GUI.contentColor = Color.black;
 
 		if (ShowButton) {
 			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 50, 100, 100), "START")) {
@@ -36,20 +42,20 @@ public class Menu : MonoBehaviour {
 		}
 
 		//once the start game button has been selected - do this
-		if (!ShowButton) {
-			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 3 - 50, Screen.height / 2 + 50, 100, 50), "1 PLAYER")) {
+		if (!ShowButton) {	
+			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 3 - 100, Screen.height / 2, 200, 100), p1)) {
 				GameSettings.Instance.players = 1;
 				start = true;
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 6 - 50, Screen.height / 2 + 50, 100, 50), "2 PLAYER")) {
+			if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 9 - 100, Screen.height / 2, 200, 100), p2)) {
 				GameSettings.Instance.players = 2;
 				start = true;
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 6 - 50, Screen.height / 2 + 50, 100, 50), "3 PLAYER")) {
+			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 9 - 100, Screen.height / 2, 200, 100), p3)) {
 				GameSettings.Instance.players = 3;
 				start = true;
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 3 - 50, Screen.height / 2 + 50, 100, 50), "4 PLAYER")) {
+			if (GUI.Button (new Rect (Screen.width / 2 + Screen.width / 3 - 100, Screen.height / 2, 200, 100), p4)) {
 				GameSettings.Instance.players = 4;
 				start = true;
 			}
