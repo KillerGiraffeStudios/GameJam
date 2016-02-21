916e2bc33ecf0c160e8fa9b14cebb63331a04581
Darvin Zhang <ddz369@mail.usask.ca>
2016-02-21 03:15:17 -0600

    Health bar now animates and moves on top of sprites in Alex's Scene
    +Health bar image
    +Health bar movement testing
    +Scaling Health

--- GameJam2016/Assets/Scripts/HealthBar.cs
+++ GameJam2016/Assets/Scripts/HealthBar.cs
@@ -11,29 +11,26 @@ public class HealthBar : MonoBehaviour {
 	//initializes the health bar as well as the maxHP of what the object originally had
 	//and its current hp as the same
 	void Start () {
-		aTexture = Resources.Load ("black") as Texture;
-		maxHP = 100;
-		curHP = 100;
-		//maxHP = GetComponent<PlayerClass>().health; 
-		//curHP = GetComponent<PlayerClass>().health; 
+		aTexture = Resources.Load ("health") as Texture;
+		maxHP = GetComponent<PlayerClass>().health; 
+		curHP = GetComponent<PlayerClass>().health; 
 		percentOfHP = maxHP / curHP;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		//move the health bar to the object
-		pos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x + xOffset, transform.position.y + yOffset,transform.position.z));
+		pos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x - 1, transform.position.y + 1, transform.position.z));
 		pos.y = Screen.height - pos.y;
-		//pos.x += xOffset;
 		//check if any damage has been done, if so, reduce hp
-		//curHP = GetComponent<PlayerClass>().health; 
+		//curHP = GetComponent<PlayerClass>().health;  --uncomment this for hp going down
 		curHP -= 1;
 		percentOfHP = curHP / maxHP;
 	}
 
 	void OnGUI(){
 		if (curHP > 0) {
-			GUI.DrawTexture(new Rect(pos.x,pos.y, percentOfHP * 100, 40), aTexture, ScaleMode.ScaleToFit, true, 10.0F);
+			GUI.DrawTexture(new Rect(pos.x,pos.y, percentOfHP * Screen.width/14, Screen.height/50), aTexture, ScaleMode.StretchToFill, true, 10.0F);
 		}
 	}
 }
