cebb6c7513887359e854af04c77e77097b7737dd
Darvin Zhang <ddz369@mail.usask.ca>
2016-02-20 23:06:19 -0600

    Added a health script
    +Added a general health script (needs testing)

---
+++ GameJam2016/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	//the postition and hp of the object
+	public Vector3 pos;
+	public float xOffset, yOffset, curHP, maxHP, percentOfHP;
+	public Texture aTexture;
+
+	// Use this for initialization
+	//initializes the health bar as well as the maxHP of what the object originally had
+	//and its current hp as the same
+	void Start () {
+		aTexture = Resources.Load ("black") as Texture;
+		maxHP = GetComponent<PlayerClass>().health; 
+		curHP = GetComponent<PlayerClass>().health; 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//move the health bar to the object
+		pos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x + xOffset, transform.position.y + yOffset,transform.position.z));
+		pos.y = Screen.height - pos.y;
+		pos.x += xOffset;
+		//check if any damage has been done, if so, reduce hp
+		curHP = GetComponent<PlayerClass>().health; 
+		percentOfHP = curHP / maxHP;
+	}
+
+	void OnGUI(){
+		if (curHP > 0) {
+			GUI.DrawTexture(new Rect(pos.x,pos.y, percentOfHP * 100, 40), aTexture, ScaleMode.ScaleToFit, true, 10.0F);
+		}
+	}
+}
