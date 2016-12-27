cab9ee6fd97afc0e46db5ac67179de9c7751c825
aj3x <ajrussell94@hotmail.com>
2016-12-27 10:59:13 -0600

    Wizard Flame Fix

--- GameJam2016/Assets/Scripts/Flame.cs
+++ GameJam2016/Assets/Scripts/Flame.cs
@@ -5,8 +5,6 @@ public class Flame : MonoBehaviour {
 	CircleCollider2D circ;
 	// Use this for initialization
 	void Start () {
-		circ = GetComponent<CircleCollider2D> ();
-		circ.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -17,14 +15,6 @@ public class Flame : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll){
 		coll.SendMessage ("damage", 30);
-	}
-	public void activate(){
-		circ.enabled = false;
-		Invoke ("deactivate", 0.3f);
-	}
-
-	public void deactivate(){
-		circ.enabled = false;
 	}
 
 }
